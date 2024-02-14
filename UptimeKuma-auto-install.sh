a812058b725c069cf426700edd11c8f81916f865
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2024-02-15 00:06:44 +0300

    Update UptimeKuma-auto-instal.sh

--- UptimeKuma-auto-install.sh
+++ UptimeKuma-auto-install.sh
@@ -4,16 +4,16 @@ sudo apt install -y \
     curl \
     gnupg \
     lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -y | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
-apt install docker-compose
-mkdir kuma
+sudo apt update -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-compose -y
+sudo mkdir kuma
 cd kuma
-mkdir data
+sudo mkdir data
 cat <<EOT >> docker-compose.yml
 version: "3.1"
 
