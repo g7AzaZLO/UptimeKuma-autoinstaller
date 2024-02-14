26a3399952e0faf4f982f578f73d6ae32970dcec
ZLO#DEV <59707245+g7AzaZLO@users.noreply.github.com>
2024-02-15 00:12:40 +0300

    Update and rename UptimeKuma-auto-install.sh to Uptime-Kuma-auto-install.sh

--- Uptime-Kuma-auto-install.sh
+++ Uptime-Kuma-auto-install.sh
@@ -4,7 +4,7 @@ sudo apt install -y \
     curl \
     gnupg \
     lsb-release
-yes | curl -fsSL https://download.docker.com/linux/ubuntu/gpg | yes | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -Y
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
